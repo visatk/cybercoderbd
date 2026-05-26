cce9115cc34c3d7779cd1058f3ffa00ec62754ca
visatk <visatk29@gmail.com>
2026-05-26 22:12:10 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { 
   Download, Search, AlertCircle, PlayCircle, Zap, Shield, Globe2, 
-  HelpCircle, ArrowRight
+  CheckCircle2, ArrowRight
 } from 'lucide-react';
 
 interface Subtitle {
